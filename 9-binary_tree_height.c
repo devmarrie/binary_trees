8b53ac0d53bf68e709a526803b24bb85c5091583
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-17 22:09:57 +0300

    [fix]:bug fixes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t h_left = 0, h_right = 0;
     if (tree)
     {
-        size_t h_left = 0, h_right = 0;
-        h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        
+        h_left = (tree->left) ? 1 + binary_tree_height(tree->left) : 1;
+        h_right = (tree->right) ? 1 + binary_tree_height(tree->right) : 1;
 
         return (h_left > h_right) ? h_left : h_right;  
     }
