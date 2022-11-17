b4397f86c688382b8a1d76108a3efa142e34b053
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-17 12:10:32 +0300

    [feat]: insert on the right side

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,23 +13,24 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
     binary_tree_t *new;
-    if(parent == NULL)
+
+	if (parent == NULL)
     {
         return NULL;
     }
 
-    new = binary_tree_node(parent,value);
-    if(new == NULL)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
     {
         return NULL;
     }
 
-    if(parent->right != NULL)
-    {
-        new->right = parent;
-        parent->right-> parent = new;
-    }
-    parent->right = new;
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
 
-    return new;
+	return new;
 }
