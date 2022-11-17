b380f06a8f5b808082b7910d99e4ace497b87999
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-17 22:10:44 +0300

    [fix]:bg fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,11 +23,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t h_left = 0, h_right = 0;
     if (tree)
     {
-        size_t h_left = 0, h_right = 0;
-        h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        
+        h_left = (tree->left) ? 1 + binary_tree_height(tree->left) : 1;
+        h_right = (tree->right) ? 1 + binary_tree_height(tree->right) : 1;
 
         return (h_left > h_right) ? h_left : h_right;  
     }
